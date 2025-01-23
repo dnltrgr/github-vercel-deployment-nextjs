26d2cd516298c80331b4fdc61fd31dc26cae0e39
Daniel Tröger <126077473+dnltrgr@users.noreply.github.com>
2025-01-23 15:11:28 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
             <li>
               Get started by editing <code>src/pages/index.js</code>.
             </li>
-            <li>Everything worked well! Vercel Preview Deployment REALLY done!!!</li>
+            <li>Everything worked well! Vercel Preview Deployment REALLY done!!!!</li>
           </ol>
 
           <div className={styles.ctas}>
